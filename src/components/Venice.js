554fe31687f511dd59379a642d6cd1682d8c0b67
BrandonMager <sotogarciabrandon@gmail.com>
2022-08-18 19:28:18 -0700

    First Commit

--- src/components/Venice.js
+++ src/components/Venice.js
@@ -3,9 +3,9 @@ export default function Venice() {
     return(
         <div className ="flex flex-col bg-[#1881ab]">
             
-            <section className = "mx-auto mt-5 bg-hero bg-cover px-32 py-32">
+            <section className = "sm:p-32 mx-auto mt-5 bg-hero bg-cover p-10">
             <div className = "text-white bg-sky-900 opacity-75 rounded-xl">
-                <h1 className = "font-medium text-2xl px-10 py-10 opacity-100">Weather Station for Venice High School</h1>
+                <h1 className = "font-medium text-2xl text-center sm:p-10 p-5 opacity-100">Weather Station for Venice High School</h1>
             </div>
             
         </section>
